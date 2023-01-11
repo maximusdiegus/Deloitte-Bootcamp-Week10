37c717f389d85ff1b2bd0594448c61d79a41d0d2
diegusDeveloper <diegusdeveloper@gmail.com>
2023-01-11 12:30:12 +0000

    main updated

--- modulesDiffAMIs/main.tf
+++ modulesDiffAMIs/main.tf
@@ -15,8 +15,8 @@ variable "type" {
 variable "zone" {
     description = "map of availability zones for eu-west-2"
     default     = {
-        1 = "eu-west-2a"
-        2 = "eu-west-2b"
+        1 = {az = "eu-west-2a", ami = "ami-084e8c05825742534"}
+        2 = {az = "eu-west-2b", ami = "ami-03542b5588cd0e6b3"}
     }
 }
 
